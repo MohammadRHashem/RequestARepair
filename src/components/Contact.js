9f5627a5bb9fad1c29c4914a35bf5cd2c43e5a80
Mohammad Hashem <mohamedhashem313@outlook.com>
2024-08-03 13:13:07 +0300

    Changed form behavior in certain cases
    - added combo select for service type
    - upgraded style for a smoother and better experience
    - selecting (other) in services will require filling the notes textarea

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,12 +6,21 @@ const Contact = () => {
     name: "",
     location: "",
     deviceType: "",
-    service: "",
+    service: [],
     note: "",
   });
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    if (name === "service") {
+      const selectedOptions = Array.from(e.target.selectedOptions).map(
+        (option) => option.value
+      );
+      setFormData({ ...formData, [name]: selectedOptions });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleSubmit = (e) => {
@@ -19,18 +28,21 @@ const Contact = () => {
 
     const { name, location, deviceType, service, note } = formData;
 
-    // Replace the phone number with your WhatsApp number
+    if (service.includes("Other") && note.trim() === "") {
+      alert("Please provide additional information in the note section.");
+      return;
+    }
+
     const phoneNumber = "9613907054";
 
-    // Constructing the message
-    const message = `*##New customer repair request##*\n\n*Name*: ${name}\n*Location*: ${location}\n*Device Type*: ${deviceType}\n*Service*: ${service}\n*Note*: ${note}`;
+    const message = `*##New customer repair request##*\n\n*Name*: ${name}\n*Location*: ${location}\n*Device Type*: ${deviceType}\n*Service*: ${service.join(
+      ", "
+    )}\n*Note*: ${note}`;
 
-    // Creating the WhatsApp URL
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
       message
     )}`;
 
-    // Redirecting to WhatsApp
     window.location.href = whatsappURL;
   };
 
@@ -38,7 +50,7 @@ const Contact = () => {
     <form className="form" onSubmit={handleSubmit}>
       <h1>Book a Repair Service</h1>
 
-      <label>Name</label>
+      <label>Name | الإسم</label>
       <input
         required
         type="text"
@@ -48,7 +60,7 @@ const Contact = () => {
         onChange={handleChange}
       />
 
-      <label>Where do you live?</label>
+      <label>Address | مكان السكن</label>
       <select
         required
         name="location"
@@ -62,7 +74,7 @@ const Contact = () => {
         <option value="Other">Other</option>
       </select>
 
-      <label>Device Type</label>
+      <label>Device Type | نوع الجهاز</label>
       <select
         required
         name="deviceType"
@@ -73,15 +85,17 @@ const Contact = () => {
           choose your device type
         </option>
         <option value="Laptop">Laptop</option>
-        <option value="Desktop">DesktopPC</option>
+        <option value="Desktop">Desktop</option>
       </select>
 
-      <label>Service</label>
+      <label>Service(s) | نوع الخدمة</label>
       <select
         required
         name="service"
         value={formData.service}
         onChange={handleChange}
+        multiple
+        
       >
         <option value="" disabled>
           choose a service:
@@ -91,18 +105,19 @@ const Contact = () => {
         <option value="Reset Windows Password">Reset Windows password</option>
         <option value="Fix a Problem">Fix a problem</option>
         <option value="Activate Windows">Activate Windows</option>
-        <option value="Other">Other (mention in the notes..)</option>
+        <option value="Other">Other (mention it below..)</option>
       </select>
 
-      <label>Note</label>
+      <label>Note or extra info | ملاحظة أو معلومة إضافية</label>
       <textarea
         name="note"
-        placeholder="Message"
+        placeholder="type here"
         value={formData.note}
         onChange={handleChange}
+        required={formData.service.includes("Other")}
       ></textarea>
 
-      <button type="submit">Send Request</button>
+      <button type="submit">Send | إرسال</button>
     </form>
   );
 };
